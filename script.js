d048ea8be50abf294a11e5793705781930e62982
“soczynskiT <t.soczynski86@gmail.com>
2018-05-23 16:43:21 +0200

    Commit 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://limitless-scrubland-93231.herokuapp.com/v1/tasks/';
-  const trelloApiRoot = 'http://limitless-scrubland-93231.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://limitless-scrubland-93231.herokuapp.com/v1/tasks/';
+  const trelloApiRoot = 'https://limitless-scrubland-93231.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
